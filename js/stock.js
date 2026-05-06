60a1167832b7643ecf02ae684330c44e65541353
abhishekgautam8260-ux <abhishekgautam8260@gmail.com>
2026-05-06 22:21:46 +0530

    Update stock.js

--- js/stock.js
+++ js/stock.js
@@ -2,7 +2,7 @@ const raw = localStorage.getItem("selectedStock");
 const stockData = raw ? JSON.parse(raw) : null;
 
 const startPrice = stockData?.price ? Number(stockData.price) : 500;
-const API = "https://proactive-essence-production.up.railway.app";
+const API = "https://backend-production-1150.up.railway.app";
 
 let prices = [];
 let current = startPrice;
