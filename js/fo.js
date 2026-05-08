892fbf8b89ca5446a2e806b7d2bd79ccad032326
abhishekgautam8260-ux <abhishekgautam8260@gmail.com>
2026-05-08 15:38:20 +0530

    Add files via upload

---
+++ js/fo.js
@@ -0,0 +1,13 @@
+// WALLET LOAD
+
+const user = JSON.parse(localStorage.getItem("user")) || {};
+
+const wallet = Number(user.walletBalance || 0);
+
+document.getElementById("walletAmount").innerText = wallet.toFixed(1);
+
+/* GO HOME */
+
+function goToHome() {
+  window.location.href = "newindex.html";
+}
